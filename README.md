1f5558c29f6362dcb04b7715641839f4c1a65520
Maximilian <Maximilian@gaengele.de>
2020-03-15 22:30:02 +0100

    Initial Upload

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-ServerDowntimeTelegramBot
+# ServerDowntimeTelegramBot
+
+## What is this?
+
+This is a little application that will notify you via Telegram whenever the targetserver is not reachable anymore.
+It will check for:
+
+- HTTP/HTTPS sites
+- pingable IPs
+- open RDP Port
+
+## How to use
+
+1. Create a Telegram bot by messaging @BotFather on Telegram
+2. Allow the Bot to text you by messaging your created bot on Telegram
+3. Get your Chat_Id by messaging @userinfobot on Telegram
+4. Create a schedule in the Windows scheduler to run it every minute
+
+These are the parameters you need to run the Bot
+- PATH_TO_PROGRAM.EXE
+- Bot Token from your created Bot (@BotFather tells you your Token)
+- Any Server Name you want to give it
+- The IP Address you want to check
+- An infinite Amount of Chat_Ids you got from @userinfobot
+
+### Example
+
+    ServerDowntimeTelegramBot.exe "1022838193:AXE-p8-u624Tv3NxYHRsTVDz614s2klcvsf" "Google Germany" "216.58.207.67" "514300732" "592310342"
+
+This would try to ping the German Google Servers and send a Message to "514300732" and "592310342" when they are down.
---
+++ ServerDowntimeTelegramBot/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+
+namespace ServerDowntimeTelegramBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 4)
+                return;
+
+            Directory.CreateDirectory("Saves");
+
+            string token = args[0];
+            string serverName = args[1];
+            string ip = args[2];
+            if (ip.Contains("//"))
+                ip = ip.Split(new string[] { "//" }, 2, StringSplitOptions.None)[1];
+
+            string[] targets = args.Skip(3).ToArray();
+
+            string savePath = "Saves/" + ReplaceInvalidChars(serverName) + ".save";
+            bool saveExists = File.Exists(savePath);
+
+            //IF NO SERVICE ANSWERS ITS DOWN!
+            if (!isReachablePing(ip) && !isPortOpen(ip, 1433) && !isReachableHttp(ip))
+            {
+                if (!saveExists)
+                {
+                    var botClient = new TelegramBotClient(token);
+                    foreach (string target in targets)
+                    {
+                        Task.Run(async () =>
+                        {
+                            await botClient.SendTextMessageAsync(
+                              chatId: target,
+                              text: "⚠️ THE SERVER \"" + serverName + "\" IS NOT REACHABLE! ⚠️"
+                            );
+                        }).GetAwaiter().GetResult();
+                    }
+
+                    File.Create(savePath);
+                }
+            }
+            else if (saveExists)
+            {
+                File.Delete(savePath);
+            }
+        }
+
+        static bool isReachableHttp(string ip)
+        {
+            if (!ip.ToLower().StartsWith("http"))
+                ip = "http://" + ip;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(ip);
+            request.Timeout = 10000;
+            request.Method = "HEAD";
+            try
+            {
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                {
+                    return response.StatusCode == HttpStatusCode.OK;
+                }
+            }
+            catch (WebException)
+            {
+                return false;
+            }
+        }
+        static bool isReachablePing(string ip)
+        {
+            Ping ping = new Ping();
+            return ping.Send(ip).Status == IPStatus.Success;
+        }
+
+        static bool isPortOpen(string ip, int port)
+        {
+            using (TcpClient tcpClient = new TcpClient())
+            {
+                try
+                {
+                    tcpClient.Connect(ip, port);
+                    return true;
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+            return false;
+        }
+
+        static string ReplaceInvalidChars(string filename)
+        {
+            return string.Join("_", filename.Split(Path.GetInvalidFileNameChars()));
+        }
+    }
+}
